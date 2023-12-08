469142e21b1d579c34fb726baffc3212953c14ea
Zackaria Osman <Zackaria.Osman@edu.sait.ca>
2023-12-07 17:49:45 -0700

    Got the API to work

--- app/page.js
+++ app/page.js
@@ -1,9 +1,40 @@
 
+'use client';
+import React, { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    const [reference, setReference] = useState('');
+    const [edition, setEdition] = useState('en.asad');
+    const [verse, setVerse] = useState('');
+
+    const fetchVerse = async () => {
+        const url = `http://api.alquran.cloud/v1/ayah/${reference}/${edition}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.status === 'OK') {
+            setVerse(data.data.text);
+        } else {
+            setVerse('Verse not found. Please check the reference or edition.');
+        }
+    };
+
+    return (
+        <div className="container">
+            <h1>Quran Verse Retrieval</h1>
+            
+            <input 
+                type="text"
+                value={reference}
+                onChange={(e) => setReference(e.target.value)}
+                placeholder="Enter Verse (e.g., 2:255)"
+            />
+            <select value={edition} onChange={(e) => setEdition(e.target.value)}>
+                <option value="en.asad">English Translation</option>
+                <option value="ar.alafasy">Arabic</option>
+            </select>
+            <button onClick={fetchVerse}>Fetch Verse</button>
+
+            {verse && <div className="verse">{verse}</div>}
+        </div>
+    );
 }
