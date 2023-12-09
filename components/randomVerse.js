4e0e8ea853bf838ca5c19d4e9d77ba52c7c27b2a
Zackaria Osman <Zackaria.Osman@edu.sait.ca>
2023-12-08 22:29:55 -0700

    Update randomVerse.js

--- components/randomVerse.js
+++ components/randomVerse.js
@@ -64,7 +64,7 @@ export default function RandomVerse() {
                 </div>
             </div>
         
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded fixed bottom-10 left-1/2 transform -translate-x-1/2" onClick={fetchRandomVerse}>Fetch Random Verse</button>
+            <button className=" my-11 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded fixed bottom-10 left-1/2 transform -translate-x-1/2" onClick={fetchRandomVerse}>Fetch Random Verse</button>
         </div>
     );
     
